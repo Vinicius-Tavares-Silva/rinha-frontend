65d6ca9a6e628ffd7c4246db57c4abdd1f0c34b1
Vinicius Tavares <vinicius.silva@omie.com.br>
2023-10-14 14:03:42 -0300

    install Vue + tailwindcss

---
+++ rinha-frontend/src/App.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <header>
+    <h3>Rinha FrontEnd</h3>
+  </header>
+
+  <main>
+    <span>Começando</span>
+  </main>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
